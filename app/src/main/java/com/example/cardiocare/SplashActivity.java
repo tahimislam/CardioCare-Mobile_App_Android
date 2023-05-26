536b751f4b70a0978f6c9b1267ce307e69450f9d
tahimislam <tahimislam16@gmail.com>
2023-05-26 18:35:19 +0600

    2nd commit

---
+++ app/src/main/java/com/example/cardiocare/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.cardiocare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.example.cardiocare.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    ActivitySplashBinding binding;
+    Animation topAni,botAni;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        topAni= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        botAni=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        binding.appicon.setAnimation(topAni);
+        binding.appname.setAnimation(botAni);
+
+
+        //create object of Handler class object
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+    }
+}
