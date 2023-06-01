0a6b99f7c23858e75a0df734e8b246554610289b
Md Robiul Islam <islamrobiul5444@gmail.com>
2023-06-01 22:31:23 +0600

    Add new feature

--- app/src/main/java/com/example/cardiocare/MainActivity.java
+++ app/src/main/java/com/example/cardiocare/MainActivity.java
@@ -1,14 +1,123 @@
 package com.example.cardiocare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.heartify.R;
+import com.example.heartify.adaptors.RecordListAdaptor;
+import com.example.heartify.interfaces.RecordListListeners;
+import com.example.heartify.models.Record;
+import com.example.heartify.models.RecordList;
+import com.example.heartify.utils.Helpers;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.dialog.MaterialDialogs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RecordListListeners {
+
+    RecyclerView recyclerView;
+    Button createButton;
+    RecordList recordList = RecordList.getInstance();
+    static public RecordListAdaptor recordListAdaptor;
+    static TextView totalText;
+    static ImageView imageView;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.main_recycler_view);
+        createButton = findViewById(R.id.main_create_button);
+        totalText = findViewById(R.id.activity_main_total);
+        imageView = findViewById(R.id.activity_main_empty_image);
+        context = this;
+
+        recordList.setRecords(Helpers.getRecordsFromDB(MainActivity.this));
+
+        recordListAdaptor = new RecordListAdaptor(this, recordList.getRecords(), this);
+        recyclerView.setAdapter(recordListAdaptor);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, COERecordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        setTotal();
+
     }
+
+    @SuppressLint("SetTextI18n")
+    static void setTotal() {
+        if (RecordList.getInstance().getCount() == 0) {
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
+        totalText.setText("Total results found: " + String.valueOf(RecordList.getInstance().getCount()));
+    }
+
+
+    @Override
+    public void onRecordDeleteListener(int position) {
+        showDeleteDialog(position);
+    }
+
+    private void showDeleteDialog(int position) {
+
+        final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+        View view = getLayoutInflater().inflate(R.layout.record_delete_dialog_layout, null);
+        Button confirmButton, cancelButton;
+        confirmButton = view.findViewById(R.id.delete_record_dialog_confirm_button);
+        cancelButton = view.findViewById(R.id.delete_record_dialog_cancel_button);
+        alert.setView(view);
+        final AlertDialog alertDialog = alert.create();
+        alertDialog.setCanceledOnTouchOutside(true);
+        alertDialog.show();
+        //Delete the record
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onClick(View view) {
+                //deleting the item
+                RecordList.getInstance().deleteRecord(position);
+                //notifying the listener
+                recordListAdaptor.notifyItemRemoved(position);
+                recordListAdaptor.notifyItemRangeChanged(position, RecordList.getInstance().getCount());
+                //showing a toast to the user
+                Toast.makeText(MainActivity.this, "Record deleted", Toast.LENGTH_SHORT).show();
+                //dismissing the alert
+                alertDialog.dismiss();
+                //setting total text
+                setTotal();
+                //saving data to database
+                Helpers.setRecordsToDB(MainActivity.this, RecordList.getInstance().getRecords());
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/cardiocare/RecordListAdaptor.java
@@ -0,0 +1,140 @@
+package com.example.cardiocare;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.heartify.activities.COERecordActivity;
+import com.example.heartify.activities.RecordDetailsActivity;
+import com.example.heartify.utils.Helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecordListAdaptor extends RecyclerView.Adapter<RecordListAdaptor.RecordListViewHolder> {
+
+    List<Record> records = new ArrayList<>();
+    Context context;
+    private RecordListListeners recordListListeners;
+
+    /**
+     * This is a constructor to for the adaptor
+     * @param context, records, recordListListener
+     */
+    public RecordListAdaptor(Context context, List<Record> records, RecordListListeners recordListListeners) {
+        this.records = records;
+        this.context = context;
+        this.recordListListeners = recordListListeners;
+    }
+
+    /**
+     * This is a method to for the adaptor
+     * @param parent, viewType
+     *                This binds the layout to the recycler view
+     */
+    @NonNull
+    @Override
+    public RecordListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.record_recyler_item_view,parent,false);
+        return new RecordListViewHolder(view);
+    }
+
+    /**
+     * This is a method to for the adaptor
+     * @param holder, position
+     *                This allows to access the views and do certain operations
+     */
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull RecordListViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.dateMeasured.setText(records.get(position).getDateMeasured());
+        holder.sysPressure.setText(records.get(position).getSystolicPressure()+" mmHg");
+        holder.diaPressure.setText(records.get(position).getDiastolicPressure()+" mmHg");
+        holder.heartRate.setText(records.get(position).getHeartRate()+" bits/min");
+        holder.status.setText(Helpers.getStatus(
+                records.get(position).getSystolicPressure(),
+                records.get(position).getDiastolicPressure()));
+        holder.statusBackground.setBackgroundColor(Color.parseColor(Helpers.getStatusColor(
+                records.get(position).getSystolicPressure(),
+                records.get(position).getDiastolicPressure())));
+
+        //on details record click
+        holder.viewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, RecordDetailsActivity.class);
+                intent.putExtra("index",position);
+
+                context.startActivity(intent);
+            }
+        });
+
+        //on edit record click
+        holder.editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Toast.makeText(context, "okay" + String.valueOf(position), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, COERecordActivity.class);
+
+                intent.putExtra("index",position);
+                context.startActivity(intent);
+            }
+        });
+
+        //on delete record click
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recordListListeners.onRecordDeleteListener(position);
+            }
+        });
+    }
+
+    /**
+     * This is a method to get the size of the list in the adaptor
+     *       This allows to access the views and do certain operations
+     */
+
+    @Override
+    public int getItemCount() {
+        return records.size();
+    }
+
+    /**
+     * This is a class to connect the views with the local variables
+     */
+    public class RecordListViewHolder extends RecyclerView.ViewHolder{
+        TextView sysPressure,diaPressure,heartRate,dateMeasured,status;
+        Button editButton,deleteButton,viewButton;
+        ConstraintLayout statusBackground;
+        public RecordListViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //connecting views by their ids
+            sysPressure = itemView.findViewById(R.id.r_r_v_i_systolic_data);
+            diaPressure = itemView.findViewById(R.id.r_r_v_i_diastolic_data);
+            heartRate = itemView.findViewById(R.id.r_r_v_i_heart_rate_data);
+            dateMeasured= itemView.findViewById(R.id.r_r_v_i_dateMeasured);
+            statusBackground = itemView.findViewById(R.id.r_r_v_i_status_background);
+            status = itemView.findViewById(R.id.r_r_v_i_status_data);
+            viewButton = itemView.findViewById(R.id.r_r_v_i_view_button);
+            editButton = itemView.findViewById(R.id.r_r_v_i_edit_button);
+            deleteButton = itemView.findViewById(R.id.r_r_v_i_delete_button);
+
+        }
+    }
+
+}
