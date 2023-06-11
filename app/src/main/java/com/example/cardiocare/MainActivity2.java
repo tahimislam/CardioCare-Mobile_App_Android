12070e14ff92f1603fd539647e22ab678e1d3ae3
Md Robiul Islam <islamrobiul5444@gmail.com>
2023-06-12 02:24:48 +0600

    2nd commit

--- app/src/main/java/com/example/cardiocare/MainActivity2.java
+++ app/src/main/java/com/example/cardiocare/MainActivity2.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 }
