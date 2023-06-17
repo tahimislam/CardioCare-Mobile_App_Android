c43216fdc20512c34570a987fa4feb4b75ea93af
tahimislam <tahimislam16@gmail.com>
2023-06-17 17:41:50 +0600

    7th commit

--- app/src/main/java/com/example/cardiocare/InsertRecordActivity.java
+++ app/src/main/java/com/example/cardiocare/InsertRecordActivity.java
@@ -1,8 +1,8 @@
 package com.example.cardiocare;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class InsertRecordActivity extends AppCompatActivity {
 
